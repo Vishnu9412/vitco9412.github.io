42478dd1669f1c1383a4089b626b5b01cccd34b3
Vishnu9412 <87561982+Vishnu9412@users.noreply.github.com>
2022-05-25 12:26:52 +0530

    adding loading

--- main.js
+++ main.js
@@ -22,6 +22,6 @@ app.get('/products', (req, res) => {
 })
 
 app.listen(port, () => {
-    // console.log(`Example app listening on port ${port}`)
-    // console.log(`click here to visit : http://localhost:${port}`)
+    console.log(`Example app listening on port ${port}`)
+    console.log(`click here to visit : http://localhost:${port}`)
 })
